05353fcd43ca0306444081074c7d625fddbd7edb
Daniel <daniel.tello@usil.pe>
2023-02-05 01:05:33 -0500

    correo2

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php  
+
+$destino= "danieltelloa@hotmail.com";
+$nombre= $_POST["nombre"];
+$correo= $_POST["correo"];
+$telefono= $_POST["telefono"];
+$mensaje= $_POST["mensaje"];
+
+
+
+
+?>  
+
